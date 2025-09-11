7e6cd870f13513f791065005bc6f36dff72ab53e
Andres <andresf.gacharnat@utadeo.edu.co>
2025-09-11 11:01:23 -0500

    feat: All Section 12 Finished

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     }),
   );
 
-  console.log('Hola mundo, segundo cambiooo');
+  console.log('Cambio definitivo');
   await app.listen();
   logger.log(`Products Microservice running on port ${envs.port}`);
 }
